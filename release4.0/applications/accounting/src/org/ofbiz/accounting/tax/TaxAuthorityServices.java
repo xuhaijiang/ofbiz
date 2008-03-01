xuhaijiang/ofbiz

commit 160ca6e20a212a2f246d45450f8f03689c4ca59a

Merged by hand from trunk 607359
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@608352 13f79535-47bb-0310-9956-ffa450edef68

release4.0/applications/accounting/src/org/ofbiz/accounting/tax/TaxAuthorityServices.java
@@ -53,9 +53,12 @@ import org.ofbiz.service.ServiceUtil;
 public class TaxAuthorityServices {
 
     public static final String module = TaxAuthorityServices.class.getName();
-    public static final BigDecimal ZERO_BASE = new BigDecimal("0.000"); 
-    public static final BigDecimal ONE_BASE = new BigDecimal("1.000"); 
+    public static final BigDecimal ZERO_BASE = BigDecimal.ZERO; 
+    public static final BigDecimal ONE_BASE = BigDecimal.ONE; 
     public static final BigDecimal PERCENT_SCALE = new BigDecimal("100.000"); 
+    public static int salestaxFinalDecimals = UtilNumber.getBigDecimalScale("salestax.final.decimals");
+    public static int salestaxCalcDecimals = UtilNumber.getBigDecimalScale("salestax.calc.decimals");
+    public static int salestaxRounding = UtilNumber.getBigDecimalRoundingMode("salestax.rounding");
 
     public static Map rateProductTaxCalcForDisplay(DispatchContext dctx, Map context) {
         GenericDelegator delegator = dctx.getDelegator();
@@ -68,9 +71,6 @@ public class TaxAuthorityServices {
 
         if (quantity == null) quantity = ONE_BASE;
         BigDecimal amount = basePrice.multiply(quantity);
-        int salestaxFinalDecimals=UtilNumber.getBigDecimalScale("salestax.final.decimals");
-        int salestaxCalcDecimals=UtilNumber.getBigDecimalScale("salestax.calc.decimals");
-        int salestaxRounding=UtilNumber.getBigDecimalRoundingMode("salestax.rounding");
         
         BigDecimal taxTotal = ZERO_BASE;
         BigDecimal taxPercentage = ZERO_BASE;
@@ -182,7 +182,7 @@ public class TaxAuthorityServices {
             // this is an add and not an addAll because we want a List of Lists of GenericValues, one List of Adjustments per item
             itemAdjustments.add(taxList);
         }
-        if (orderShippingAmount.doubleValue() > 0) {
+        if (orderShippingAmount != null && orderShippingAmount.compareTo(BigDecimal.ZERO) > 0) {
             List taxList = getTaxAdjustments(delegator, null, productStore, payToPartyId, billToPartyId, taxAuthoritySet, ZERO_BASE, ZERO_BASE, orderShippingAmount);
             orderAdjustments.addAll(taxList);
         }
@@ -313,7 +313,7 @@ public class TaxAuthorityServices {
                     taxable = taxable.add(shippingAmount);
                 }
                 
-                if (taxable.doubleValue() == 0) {
+                if (taxable.compareTo(BigDecimal.ZERO) == 0) {
                     // this should make it less confusing if the taxable flag on the product is not Y/true, and there is no shipping and such
                     continue;
                 }

commit 3bd31a26882862f0c284676907d7507efb029cfb

Fix some 1.4 issues introduced in r608352
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@632508 13f79535-47bb-0310-9956-ffa450edef68

release4.0/applications/accounting/src/org/ofbiz/accounting/tax/TaxAuthorityServices.java
@@ -53,8 +53,8 @@ import org.ofbiz.service.ServiceUtil;
 public class TaxAuthorityServices {
 
     public static final String module = TaxAuthorityServices.class.getName();
-    public static final BigDecimal ZERO_BASE = BigDecimal.ZERO; 
-    public static final BigDecimal ONE_BASE = BigDecimal.ONE; 
+    public static final BigDecimal ZERO_BASE = new BigDecimal("0.000");
+    public static final BigDecimal ONE_BASE = new BigDecimal("1.000");
     public static final BigDecimal PERCENT_SCALE = new BigDecimal("100.000"); 
     public static int salestaxFinalDecimals = UtilNumber.getBigDecimalScale("salestax.final.decimals");
     public static int salestaxCalcDecimals = UtilNumber.getBigDecimalScale("salestax.calc.decimals");
@@ -182,7 +182,7 @@ public class TaxAuthorityServices {
             // this is an add and not an addAll because we want a List of Lists of GenericValues, one List of Adjustments per item
             itemAdjustments.add(taxList);
         }
-        if (orderShippingAmount != null && orderShippingAmount.compareTo(BigDecimal.ZERO) > 0) {
+        if (orderShippingAmount != null && orderShippingAmount.doubleValue() > 0) {
             List taxList = getTaxAdjustments(delegator, null, productStore, payToPartyId, billToPartyId, taxAuthoritySet, ZERO_BASE, ZERO_BASE, orderShippingAmount);
             orderAdjustments.addAll(taxList);
         }
@@ -313,7 +313,7 @@ public class TaxAuthorityServices {
                     taxable = taxable.add(shippingAmount);
                 }
                 
-                if (taxable.compareTo(BigDecimal.ZERO) == 0) {
+                if (taxable.doubleValue() == 0) {
                     // this should make it less confusing if the taxable flag on the product is not Y/true, and there is no shipping and such
                     continue;
                 }