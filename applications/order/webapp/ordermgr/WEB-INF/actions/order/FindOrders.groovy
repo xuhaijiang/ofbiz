xuhaijiang/ofbiz

commit 094645e63455c03381d21ec5b37964c6544d875a

"Applied fix from trunk for revision: 1493393" ------------------------------------------------------------------------ r1493393 | jleroux | 2013-06-15 18:22:53 +0200 (sam., 15 juin 2013) | 7 lines
A fixing patch from Sergei Biletnikov for "Search orders by good identification" https://issues.apache.org/jira/browse/OFBIZ-5031

  currentCarrierShipmentMethod = delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false);
is not correct, the result is the list, so the remedy is simple:
  currentCarrierShipmentMethod = EntityUtil.getFirst(delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false));


------------------------------------------------------------------------
?\026

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release12.04@1493399 13f79535-47bb-0310-9956-ffa450edef68

applications/order/webapp/ordermgr/WEB-INF/actions/order/FindOrders.groovy
@@ -21,6 +21,7 @@ import java.util.*;
 import java.sql.Timestamp;
 import org.ofbiz.entity.*;
 import org.ofbiz.base.util.*;
+import org.ofbiz.entity.util.*;
 
 module = "FindOrders.groovy";
 
@@ -96,7 +97,7 @@ if (shipmentMethod) {
     carrierPartyId = shipmentMethod.substring(0, shipmentMethod.indexOf("@"));
     shipmentMethodTypeId = shipmentMethod.substring(shipmentMethod.indexOf("@")+1);
     if (carrierPartyId && shipmentMethodTypeId) {
-        currentCarrierShipmentMethod = delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId]);
+        currentCarrierShipmentMethod = EntityUtil.getFirst(delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false));
         context.currentCarrierShipmentMethod = currentCarrierShipmentMethod;
     }
 }

commit f0f7471a2d506e702d8ab22f1bbcea9f8a4e55d4

Fixed java.lang.IllegalArgumentException due to wrong arguments introduced in r1493399.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release12.04@1595153 13f79535-47bb-0310-9956-ffa450edef68

applications/order/webapp/ordermgr/WEB-INF/actions/order/FindOrders.groovy
@@ -97,7 +97,7 @@ if (shipmentMethod) {
     carrierPartyId = shipmentMethod.substring(0, shipmentMethod.indexOf("@"));
     shipmentMethodTypeId = shipmentMethod.substring(shipmentMethod.indexOf("@")+1);
     if (carrierPartyId && shipmentMethodTypeId) {
-        currentCarrierShipmentMethod = EntityUtil.getFirst(delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false));
+        currentCarrierShipmentMethod = EntityUtil.getFirst(delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId]));
         context.currentCarrierShipmentMethod = currentCarrierShipmentMethod;
     }
 }