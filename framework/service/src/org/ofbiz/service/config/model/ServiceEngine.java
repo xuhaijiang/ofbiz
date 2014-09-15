xuhaijiang/ofbiz

commit a4e47f44956c0ac081bed98e7b0364f228bb915a

"Applied fix from trunk for revision: 1624809" (conflicts handled by hand) ------------------------------------------------------------------------ r1624809 | jleroux | 2014-09-14 10:37:46 +0200 (dim. 14 sept. 2014) | 1 ligne
OFBIZ-5770 - Removes the harcoded ports related with service locations introduced with OFBIZ-4794
------------------------------------------------------------------------


git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1624814 13f79535-47bb-0310-9956-ffa450edef68

framework/service/src/org/ofbiz/service/config/model/ServiceEngine.java
@@ -89,10 +89,10 @@ public final class ServiceEngine {
             for (Element serviceLocationElement : serviceLocationElementList) {
                 String location = serviceLocationElement.getAttribute("location").intern();
                 if (location.contains("localhost") && Start.getInstance().getConfig().portOffset != 0) {
-                    Integer port = 1099 + Start.getInstance().getConfig().portOffset;
-                    location = location.replace("1099", port.toString());
-                    port = 8080 + ClassLoaderContainer.portOffset; 
-                    location = location.replace("8080", port.toString());
+                    String s = location.substring(location.lastIndexOf(":") + 1);                        
+                    Integer locationPort = Integer.valueOf(s.substring(0, s.indexOf("/")));                    
+                    Integer port = locationPort + Start.getInstance().getConfig().portOffset;
+                    location = location.replace(locationPort.toString(), port.toString());
                 }
                 serviceLocations.add(new ServiceLocation(serviceLocationElement, location));
             }

framework/service/src/org/ofbiz/service/engine/XMLRPCClientEngine.java
@@ -91,9 +91,11 @@ public class XMLRPCClientEngine extends GenericAsyncEngine {
         String keyAlias  = null;
         try {
             url = ServiceConfigUtil.getEngineParameter(engine, "url");
-            if (ClassLoaderContainer.portOffset != 0) {
-                Integer port = 8080 + ClassLoaderContainer.portOffset; 
-                url = url.replace("8080", port.toString());
+            if (Start.getInstance().getConfig().portOffset != 0) {
+                String s = url.substring(url.lastIndexOf(":") + 1);                        
+                Integer rpcPort = Integer.valueOf(s.substring(0, s.indexOf("/")));
+                Integer port = rpcPort + Start.getInstance().getConfig().portOffset;
+                url = url.replace(rpcPort.toString(), port.toString());
             }
             login = ServiceConfigUtil.getEngineParameter(engine, "login");
             password = ServiceConfigUtil.getEngineParameter(engine, "password");

commit 527dd1154d5b80e8b85dac392b0a48f2a0717b38

Fixes a bug introduced with r1624814. I forgot  that r1624549 has not been backported to R13.07, hence the ClassLoaderContainer.portOffset should still be used Thanks to Adrian who warned in dev ML
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1625157 13f79535-47bb-0310-9956-ffa450edef68

framework/service/src/org/ofbiz/service/config/model/ServiceEngine.java
@@ -24,8 +24,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.ofbiz.base.container.ClassLoaderContainer;
 import org.ofbiz.base.lang.ThreadSafe;
-import org.ofbiz.base.start.Start;
 import org.ofbiz.base.util.UtilXml;
 import org.ofbiz.service.config.ServiceConfigException;
 import org.w3c.dom.Element;
@@ -88,10 +88,10 @@ public final class ServiceEngine {
             List<ServiceLocation> serviceLocations = new ArrayList<ServiceLocation>(serviceLocationElementList.size());
             for (Element serviceLocationElement : serviceLocationElementList) {
                 String location = serviceLocationElement.getAttribute("location").intern();
-                if (location.contains("localhost") && Start.getInstance().getConfig().portOffset != 0) {
+                if (location.contains("localhost") && ClassLoaderContainer.portOffset != 0) {
                     String s = location.substring(location.lastIndexOf(":") + 1);
                     Integer locationPort = Integer.valueOf(s.substring(0, s.indexOf("/")));
-                    Integer port = locationPort + Start.getInstance().getConfig().portOffset;
+                    Integer port = locationPort + ClassLoaderContainer.portOffset;
                     location = location.replace(locationPort.toString(), port.toString());
                 }
                 serviceLocations.add(new ServiceLocation(serviceLocationElement, location));

framework/service/src/org/ofbiz/service/engine/XMLRPCClientEngine.java
@@ -91,10 +91,10 @@ public class XMLRPCClientEngine extends GenericAsyncEngine {
         String keyAlias  = null;
         try {
             url = ServiceConfigUtil.getEngineParameter(engine, "url");
-            if (Start.getInstance().getConfig().portOffset != 0) {
+            if (ClassLoaderContainer.portOffset != 0) {
                 String s = url.substring(url.lastIndexOf(":") + 1);
                 Integer rpcPort = Integer.valueOf(s.substring(0, s.indexOf("/")));
-                Integer port = rpcPort + Start.getInstance().getConfig().portOffset;
+                Integer port = rpcPort + ClassLoaderContainer.portOffset;
                 url = url.replace(rpcPort.toString(), port.toString());
             }
             login = ServiceConfigUtil.getEngineParameter(engine, "login");